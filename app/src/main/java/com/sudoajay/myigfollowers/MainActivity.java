7d2d74723f3ad637c41267e60a540ed6de2fcf2f
sudoajay <sudoajay@gmail.com>
2019-11-08 06:41:35 +0530

    Done I guess

--- app/src/main/java/com/sudoajay/myigfollowers/MainActivity.java
+++ app/src/main/java/com/sudoajay/myigfollowers/MainActivity.java
@@ -1,19 +1,32 @@
 package com.sudoajay.myigfollowers;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.sudoajay.myigfollowers.Toast.CustomToast;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> saveBackPage = new ArrayList<>();
+    private WebView myWebView;
+    private boolean doubleBackToExitPressedOnce;
 
     @SuppressLint({"SetJavaScriptEnabled", "WrongConstant"})
     @Override
@@ -26,7 +39,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         changeStatusBarColor();
-        WebView myWebView = findViewById(R.id.myWebView);
+
+        String webPage = "https://myigfollowers.com/";
+        saveBackPage.add(webPage);
+
+        myWebView = findViewById(R.id.myWebView);
         myWebView.setPadding(0, 0, 0, 0);
         myWebView.setInitialScale(1);
         myWebView.setScrollBarStyle(33554432);
@@ -37,19 +54,86 @@ public class MainActivity extends AppCompatActivity {
             settings.setAllowFileAccessFromFileURLs(true);
             settings.setAllowUniversalAccessFromFileURLs(true);
         }
-
         settings.setUseWideViewPort(true);
         settings.setLoadWithOverviewMode(true);
-        myWebView.setWebChromeClient(new WebChromeClient());
-        myWebView.loadUrl("https://myigfollowers.com/");
+        myWebView.setWebViewClient(new CustomWebViewClient());
+        myWebView.loadUrl(webPage);
+
 
     }
+
 
     private void changeStatusBarColor() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (saveBackPage.size() > 1) {
+            myWebView.loadUrl(saveBackPage.get(saveBackPage.size() - 2));
+            saveBackPage.remove(saveBackPage.size() - 1);
+        } else {
+            if (doubleBackToExitPressedOnce) {
+                Finish();
+                return;
+            }
+
+            this.doubleBackToExitPressedOnce = true;
+            CustomToast.ToastIt(getApplicationContext(), "Click Back Again To Exit");
+
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 2000);
+        }
+    }
+
+    public void Finish() {
+        Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+        homeIntent.addCategory(Intent.CATEGORY_HOME);
+        homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(homeIntent);
+
+    }
+    class CustomWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            final Uri uri = Uri.parse(url);
+            return handleUri(uri);
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            final Uri uri = request.getUrl();
+            return handleUri(uri);
+        }
+
+        private boolean handleUri(final Uri uri) {
+            final String host = uri.getHost();
+            // Based on some condition you need to determine if you are going to load the url
+            // in your web view itself or in a browser.
+            // You can use `host` or `scheme` or any part of the `uri` to decide.
+            assert host != null;
+            if (host.equals("myigfollowers.com")) {
+                // Returning false means that you are going to load this url in the webView itself
+                saveBackPage.add(uri.toString());
+                return false;
+            } else {
+                // Returning true means that you need to handle what to do with the url
+                // e.g. open web page in a Browser
+                final Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                return true;
+            }
         }
     }
 }

d41e5e4a7fba5c352a730454601e92643d11bf92
sudoajay <sudoajay@gmail.com>
2019-11-09 16:26:45 +0530

    remove status bar bg color

--- app/src/main/java/com/sudoajay/myigfollowers/MainActivity.java
+++ app/src/main/java/com/sudoajay/myigfollowers/MainActivity.java
@@ -3,14 +3,10 @@ package com.sudoajay.myigfollowers;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -36,13 +32,8 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-
         setContentView(R.layout.activity_main);
 
-        changeStatusBarColor(1);
 
         saveBackPage.add(webPage);
 
@@ -59,7 +50,6 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         myWebView.setWebViewClient(new WebViewClient() {
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
                 myWebView.loadUrl("file:///android_asset/noInternetConnection.html");
-                changeStatusBarColor(2);
             }
         });
 
@@ -70,7 +60,6 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 
     @SuppressLint({"SetJavaScriptEnabled", "WrongConstant"})
     private void show() {
-        changeStatusBarColor(1);
 
         myWebView.setPadding(0, 0, 0, 0);
         myWebView.setInitialScale(1);
@@ -86,19 +75,6 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         settings.setLoadWithOverviewMode(true);
         myWebView.setWebViewClient(new CustomWebViewClient());
         myWebView.loadUrl(webPage);
-    }
-
-
-    private void changeStatusBarColor(int type) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            if (type == 2)
-                window.setStatusBarColor(Color.parseColor("#1c457b"));
-            else {
-                window.setStatusBarColor(Color.TRANSPARENT);
-            }
-        }
     }
 
     @Override
@@ -141,7 +117,6 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                     RunThread_Internet();
                 } else {
                     myWebView.loadUrl(webPage);
-                    changeStatusBarColor(1);
                 }
             }
         }, 5000); // 5 sec
@@ -156,10 +131,8 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
             @Override
             public void run() {
                 if (!DetectConnection.checkInternetConnection(getApplicationContext())) {
-                    changeStatusBarColor(2);
                     myWebView.reload();
                 } else {
-                    changeStatusBarColor(1);
                     myWebView.loadUrl(webPage);
                 }
                 swipeToRefresh.setRefreshing(false);
--- app/src/main/java/com/sudoajay/myigfollowers/SplashScreen/Splash.java
+++ app/src/main/java/com/sudoajay/myigfollowers/SplashScreen/Splash.java
@@ -1,16 +1,10 @@
 package com.sudoajay.myigfollowers.SplashScreen;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 
 import com.sudoajay.myigfollowers.MainActivity;
 import com.sudoajay.myigfollowers.R;
@@ -22,14 +16,10 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Making notification bar transparent
-        if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
+
 
         setContentView(R.layout.activity_splash_screen);
-        changeStatusBarColor();
-
-        int splashTimeOut = 5000;
+        int splashTimeOut = 2000;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -41,11 +31,4 @@ public class Splash extends AppCompatActivity {
 
     }
 
-    private void changeStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        }
-    }
 }
